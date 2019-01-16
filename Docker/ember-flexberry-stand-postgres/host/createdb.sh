b3b849e4d6615c6bb2a75e26fc7525c89164a039
akosinsky <akosinsky@ICS-KK12324>
2019-01-16 12:15:18 +0500

    Add inserts to postgres

--- Docker/ember-flexberry-stand-postgres/host/createdb.sh
+++ Docker/ember-flexberry-stand-postgres/host/createdb.sh
@@ -5,5 +5,6 @@ cd /docker/host
 
 until psql -U postgres <create.sql; do echo "Wait...";sleep 2; done 
 until psql -U flexberryuser -d appdb <PostgreSql.create.sql; do echo "Wait...";sleep 5; done 
+until psql -U flexberryuser -d appdb <PostgreSql.insert.sql; do echo "Wait...";sleep 5; done 
 
 /etc/init.d/postgresql stop;  while su -c psql postgres </dev/null >/dev/null 2>&1; do sleep 1; done; echo "postgresql stopped"
